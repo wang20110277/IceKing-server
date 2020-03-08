1788d4e8c835c315602e8aa1147657ea6efdf6a3
wanghuabing <461644850@qq.com>
2020-03-08 16:09:38 +0800

    优化结构

--- iceking-controller/src/main/java/iceking/base/controller/user/UserController.java
+++ iceking-controller/src/main/java/iceking/base/controller/user/UserController.java
@@ -1,15 +1,17 @@
-package iceking.base.controller;
+package iceking.base.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
     /**
      * 测试登录
      */
-    @RequestMapping("/getLoginUser")
+    @GetMapping("/getLoginUser")
     @ResponseBody
     public String getLoginUser(){
         return "success";
