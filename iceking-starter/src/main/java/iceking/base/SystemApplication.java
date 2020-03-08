59417bcbb844e90b3c507039ee0e7680b5b3ca7a
wanghuabing <461644850@qq.com>
2020-03-08 18:51:00 +0800

    多环境打包配置

--- iceking-starter/src/main/java/iceking/base/SystemApplication.java
+++ iceking-starter/src/main/java/iceking/base/SystemApplication.java
@@ -1,6 +1,5 @@
 package iceking.base;
 
-import iceking.base.utils.JavaWebTokenUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);
-        System.out.println(JavaWebTokenUtil.getToken());
     }
 }
