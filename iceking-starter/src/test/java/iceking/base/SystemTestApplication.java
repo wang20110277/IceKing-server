ebfad48a46c3accc5b17cf720edd54469d5e64d8
wanghuabing <461644850@qq.com>
2020-03-09 09:11:37 +0800

    spring测试

--- iceking-starter/src/test/java/iceking/base/SystemTestApplication.java
+++ iceking-starter/src/test/java/iceking/base/SystemTestApplication.java
@@ -1,9 +1,28 @@
 package iceking.base;
 
 import iceking.base.controller.user.UserController;
+import iceking.base.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class SystemTestApplication {
 
+    @Autowired
     private UserController userController;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Test
+    public void loginTest(){
+        User user=new User();
+        String result=userController.login(user);
+        System.out.println(result);
+    }
 }
