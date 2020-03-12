b77a90e245f1a9e3ded94166bb6f27c15ebb950e
wanghuabing <461644850@qq.com>
2020-03-12 16:11:48 +0800

    配置druid数据源

--- iceking-starter/src/main/java/iceking/base/SystemApplication.java
+++ iceking-starter/src/main/java/iceking/base/SystemApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 //@ServletComponentScan
 @SpringBootApplication(scanBasePackages = "iceking.base")
-@MapperScan("iceking.base.mapper")
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);
