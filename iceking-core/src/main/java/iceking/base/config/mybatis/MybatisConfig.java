2f14ed105fcc76707d6d9e0d488c3e1159f88c73
wanghuabing <461644850@qq.com>
2020-03-23 14:09:09 +0800

    多数据源配置

--- iceking-core/src/main/java/iceking/base/config/mybatis/MybatisConfig.java
+++ iceking-core/src/main/java/iceking/base/config/mybatis/MybatisConfig.java
@@ -1,25 +1,61 @@
 package iceking.base.config.mybatis;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import iceking.base.datasource.DynamicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @MapperScan("iceking.base.mapper")    // 扫描mapper
 public class MybatisConfig {
-    @Autowired
-    private DataSource dataSource;
+    @Bean("master")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource master() {
+        return new DruidDataSource();
+    }
+
+    @Bean("slave")
+    @ConfigurationProperties(prefix = "spring.datasource.slave")
+    public DataSource slave() {
+        return new DruidDataSource();
+    }
+
+    @Bean("dynamicDataSource")
+    public DataSource dynamicDataSource() {
+        DynamicDataSource dynamicDataSource = new DynamicDataSource();
+        Map<Object, Object> dataSourceMap = new HashMap<>(2);
+        dataSourceMap.put("master", master());
+        dataSourceMap.put("slave", slave());
+        // 将 master 数据源作为默认指定的数据源
+        dynamicDataSource.setDefaultDataSource(master());
+        // 将 master 和 slave 数据源作为指定的数据源
+        dynamicDataSource.setDataSources(dataSourceMap);
+        return dynamicDataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        // 配置事务管理, 使用事务时在方法头部添加@Transactional注解即可
+        return new DataSourceTransactionManager(dynamicDataSource());
+    }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setDataSource(dynamicDataSource());
         sessionFactory.setTypeAliasesPackage("iceking.base.entity");    // 扫描entity
 
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
